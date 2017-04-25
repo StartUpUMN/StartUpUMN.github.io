f58409de0789e79a0abdc79ccb9688bcd4a8fc99
Peter Humbert <humbe037@umn.edu>
2017-04-25 16:24:37 -0500

    move MC loader function to file
    Chrome flags it as an unsafe script

---
+++ js/mcsignup.js
@@ -0,0 +1,3 @@
+function mcsignup() {
+    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us15.list-manage.com","uuid":"b4aa83fd64d5f81ae217ad2e5","lid":"b2a8f2980e"}) })
+}
