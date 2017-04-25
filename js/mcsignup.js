bf23a19f659ea2100880fb38edd6c020eb7f5f3b
Peter Humbert <humbe037@umn.edu>
2017-04-25 16:33:02 -0500

    rollback MC changes

--- js/mcsignup.js
+++
@@ -1,4 +0,0 @@
-function mcsignup() {
-    console.log('test');
-    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us15.list-manage.com","uuid":"b4aa83fd64d5f81ae217ad2e5","lid":"b2a8f2980e"}) });
-}
