39804fd16ceab0f8c3501bd723645451d181e1cb
Peter Humbert <humbe037@umn.edu>
2017-04-25 16:30:38 -0500

    final test with embedded MC form

--- js/mcsignup.js
+++ js/mcsignup.js
@@ -1,3 +1,4 @@
 function mcsignup() {
-    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us15.list-manage.com","uuid":"b4aa83fd64d5f81ae217ad2e5","lid":"b2a8f2980e"}) })
+    console.log('test');
+    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us15.list-manage.com","uuid":"b4aa83fd64d5f81ae217ad2e5","lid":"b2a8f2980e"}) });
 }
