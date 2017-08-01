c29ea725c965b94cdab98a628fca1858832da829
Peter Humbert <humbe037@umn.edu>
2017-07-31 20:30:52 -0500

    new domain test
    would modal popup ever appear?

--- js/countdownToSwag.js
+++ js/countdownToSwag.js
@@ -1,6 +1,8 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Aug 8, 2017 17:00:00").getTime();
 
+console.log(window.location.href);
+
 // Update the count down every 1 second
 var x = setInterval(function() {
 
