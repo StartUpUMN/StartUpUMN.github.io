8faf687d00fd91ae08cb70572c1c2fcab3c9bd94
Peter Humbert <humbe037@umn.edu>
2017-08-11 23:30:45 -0500

    remove countdown from landing page
    but keep and generalize the countdown js

--- js/countdown.js
+++ js/countdown.js
@@ -17,12 +17,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdownToSwag").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
